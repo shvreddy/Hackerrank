b1c83f6761e1cf200733857b24155e8fbca1b406
Harsha <Harsha@HARSHA>
2016-07-19 17:49:16 -0700

    Add project files.

---
+++ Delete/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] LA = { 1, 2, 3, 4, 5 };
+            int k = 3, n = 5;
+            int i,j;
+
+            Console.WriteLine("before");
+
+            for (i=0; i<n; i++)
+            {
+                Console.WriteLine(LA[i]);
+            }
+
+            j = k;
+            while (j < n)
+            {
+                LA[j - 1] = LA[j];
+                j = j + 1;
+
+            }
+            n = n - 1;
+            Console.WriteLine("after");
+            for ( i = 0; i < n; i++)
+            {
+                Console.WriteLine(LA[i]);
+            }
+
+        }
+    }
+}
+
